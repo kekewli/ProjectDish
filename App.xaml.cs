51a45045425df4e69be176f92e27cd009a641fc0
kekewli <Andreytoon@mail.ru>
2026-05-13 14:03:39 +0400

    Revert "Восстановление функционала согласно ТЗ"
    This reverts commit 9fcdef250b9ab352bd9fb2f8fe322306823b7148.

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using AutoUpdaterDotNET;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ProjectDish
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AutoUpdater.Mandatory = true;
+
+            // AutoUpdater.UpdateMode = Mode.ForcedDownload; 
+
+            AutoUpdater.RunUpdateAsAdmin = false;
+
+            AutoUpdater.Start("https://github.com/kekewli/ProjectDish/releases/download/release/update.xml");
+        }
+    }
+
+}
