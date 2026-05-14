855b302bb08c90f6823729d83bd1a3d6a3a8a625
kekewli <Andreytoon@mail.ru>
2026-05-14 13:55:43 +0400

    Add App.xaml.cs and ignore local App.config

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using AutoUpdaterDotNET;
+using ProjectDish.MVVM.Models;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ProjectDish
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static UserModel CurrentUser { get; set; }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AutoUpdater.Mandatory = true;
+
+            AutoUpdater.RunUpdateAsAdmin = false;
+
+            AutoUpdater.Start("https://github.com/kekewli/ProjectDish/releases/download/release/update.xml");
+        }
+    }
+
+}
